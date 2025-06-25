87af200dfc57e339b9e731cb46cda6dce3cd020d
VladimirLevikov <v.v.levikov@gmail.com>
2025-06-25 18:56:47 +0300

    добавление скрипта

---
+++ scripts.py
@@ -0,0 +1,85 @@
+import datetime
+import random
+
+from datacenter.models import Subject, Commendation, Lesson, Mark, Schoolkid
+
+phrases = [
+    "Молодец!",
+    "Отлично!",
+    "Хорошо!",
+    "Гораздо лучше, чем я ожидал!",
+    "Ты меня приятно удивил!",
+    "Великолепно!",
+    "Прекрасно!",
+    "Ты меня очень обрадовал!",
+    "Именно этого я давно ждал от тебя!",
+    "Сказано здорово – просто и ясно!",
+    "Ты, как всегда, точен!",
+    "Очень хороший ответ!",
+    "Талантливо!",
+    "Ты сегодня прыгнул выше головы!",
+    "Я поражен!",
+    "Уже существенно лучше!",
+    "Потрясающе!",
+    "Замечательно!",
+    "Прекрасное начало!",
+    "Так держать!",
+    "Ты на верном пути!",
+    "Здорово!",
+    "Это как раз то, что нужно!",
+    "Я тобой горжусь!",
+    "С каждым разом у тебя получается всё лучше!",
+    "Мы с тобой не зря поработали!",
+    "Я вижу, как ты стараешься!",
+    "Ты растешь над собой!",
+    "Ты многое сделал, я это вижу!",
+    "Теперь у тебя точно все получится!"
+]
+
+
+def fix_marks(schoolkid: Schoolkid) -> None:
+    """
+    Функция испровляет оценки пользователя.
+
+    :param schoolkid - объект с информацией об ученике
+    """
+    bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
+    for mark in bad_marks:
+        mark.points = 5
+        mark.save()
+
+
+def remove_chastisements(schoolkid: Schoolkid) -> None:
+    """
+    Функция удаляет все замечания ученика
+
+    :param schoolkid - объект с информацией об ученике
+    """
+    remarks = Commendation.objects.filter(schoolkid=schoolkid)
+    for remark in remarks:
+        remark.delete()
+
+
+def create_commendation(schoolkid: Schoolkid, subject_title: str) -> None:
+    """
+    Функция создает похвалу указанному ученику, по указанному предмету.
+    Если похвала уже есть, выведется соответсвтующее сообющение.
+
+    :param schoolkid - объект с информацией об ученике
+    :subject_title - название  предмета по которому нужно добавить похвалу например 'Математика'
+    """
+    subject = Subject.objects.filter(title=subject_title, year_of_study=6)[0]
+    lesson = Lesson.objects.filter(group_letter=schoolkid.group_letter, subject=subject)[0]
+    now = datetime.datetime.now()
+    if Commendation.objects.filter(schoolkid=schoolkid, subject=subject, created=now):
+        print(f"По указанному предмету({subject_title}) уже есть похвала")
+    else:
+        Commendation.objects.create(text=random.choice(phrases), schoolkid=schoolkid, subject=subject,
+                                    teacher=lesson.teacher, created=now)
+
+
+if __name__ == "__main__":
+    user = Schoolkid.objects.get(full_name__contains="Фролов Иван")
+    create_commendation(user, "Музыка")
+    fix_marks(user)
+
